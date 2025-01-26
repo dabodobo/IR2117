d7318252767508259b66ef326ec5f74f435db70e
davidroman <david.roman.grijalbo@gmail.com>
2025-01-26 11:05:30 +0100

    sum_ab - Version 2

--- SUM/sum_ab.cpp
+++ SUM/sum_ab.cpp
@@ -13,6 +13,11 @@ int main(){
 	int a;
 	std::cout << "Enter a number >= 1: ";
 	std::cin >> a;
+	while(a < 1){
+	
+		std::cout << "Enter a number >= 1: ";
+		std::cin >> a;
+	}
 	int b;
 	std::cout << "Enter b number >= 1: ";
 	std::cin >> b;
