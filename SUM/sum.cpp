4f3c0b828961269a95b53111430c048587aa35be
davidroman <david.roman.grijalbo@gmail.com>
2025-01-26 10:46:12 +0100

    sum - Version 2

--- SUM/sum.cpp
+++ SUM/sum.cpp
@@ -5,11 +5,11 @@ int main(){
 	std::cout << "Enter a number: ";
 	std::cin >> num;
 	int sum = 0;
-	/*
+	
 	for(int i = 1; i <= num; i++){
 		sum+=i;
 	}
-	*/
+	
 	std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
 
 	return 0;
