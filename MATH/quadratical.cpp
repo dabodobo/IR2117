22feb3bc25c4f9799a261606a4a9d81c0fc26bd3
davidroman <david.roman.grijalbo@gmail.com>
2025-01-26 11:17:32 +0100

    Ecuacion - Version 1

---
+++ MATH/quadratical.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main(){
+	int a,b,c;
+	int sol = 0;
+	std::cout << "Enter a: ";
+	std::cin >> a;
+	std::cout << "Enter b: ";
+	std::cin >> b;
+	std::cout << "Enter c: ";
+	std::cin >> c;
+	std::cout << "Las soluciones son " << sol << std::endl;
+
+	return 0;
+}
