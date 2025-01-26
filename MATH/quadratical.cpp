41feb68ce6fea33bc852a1af4e302ca4dc34d904
davidroman <david.roman.grijalbo@gmail.com>
2025-01-26 23:11:11 +0100

    Discriminante >= 0

--- MATH/quadratical.cpp
+++ MATH/quadratical.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 
 
 int main(){
-	int a,b,c;
-	int sol = 0;
+	double a,b,c;
+	std::vector<double> sol;
+	
+	
 	std::cout << "Enter a: ";
 	std::cin >> a;
 	std::cout << "Enter b: ";
 	std::cin >> b;
 	std::cout << "Enter c: ";
 	std::cin >> c;
-	std::cout << "Las soluciones son " << sol << std::endl;
+	
+	double D = (b*b) - (4*a*c);
+	
+	if(D == 0){
+		sol = {(-b)/(2*a)};
+		std::cout << "La solucion es " << sol[0] << std::endl;
+	}
+	else if(D > 0){
+		sol = {(-b + sqrt(D))/(2*a),(-b - sqrt(D))/(2*a)};
+		std::cout << "Las soluciones son " << sol[0] << " y " << sol[1] << std::endl;
+	}
+	
+
 
 	return 0;
 }
