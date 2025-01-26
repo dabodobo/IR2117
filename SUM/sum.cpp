5c15833ebfa3d855e9b50ac22d71092a8cfa5a61
davidroman <david.roman.grijalbo@gmail.com>
2025-01-26 10:48:06 +0100

    Sum - Version 3

--- SUM/sum.cpp
+++ SUM/sum.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
+
+int suma(int num ){
+	int sum = 0;
+	for(int i = 1; i <= num; i++){
+		sum+=i;
+	}
+	return sum;
+}
+
 int main(){
 	int num;
 	std::cout << "Enter a number: ";
 	std::cin >> num;
-	int sum = 0;
-	
-	for(int i = 1; i <= num; i++){
-		sum+=i;
-	}
-	
-	std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
+	std::cout << "The sum from 1 to " << num << " is " << suma(num) << std::endl;
 
 	return 0;
 }
