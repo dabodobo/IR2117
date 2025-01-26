9d8761d8376e9314bcfa656495dc60f43cc455dd
davidroman <david.roman.grijalbo@gmail.com>
2025-01-26 11:04:13 +0100

    sum_ab - Version 1

---
+++ SUM/sum_ab.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+int suma(int a, int b ){
+	int sum = 0;
+	for(int i = a; i <= b; i++){
+		sum+=i;
+	}
+	return sum;
+}
+
+int main(){
+	int a;
+	std::cout << "Enter a number >= 1: ";
+	std::cin >> a;
+	int b;
+	std::cout << "Enter b number >= 1: ";
+	std::cin >> b;
+
+	std::cout << "The sum from " << a << " to " << b << " is " << suma(a,b) << std::endl;
+	
+
+	return 0;
+}
