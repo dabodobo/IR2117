4361a7993b979a5e2b0c85ff77e27ee7d01dd7b1
davidroman <david.roman.grijalbo@gmail.com>
2025-02-01 17:10:23 +0100

    Se mueve forward

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,8 +10,14 @@ int main(int argc, char *argv[]) {
     auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10); 
     geometry_msgs::msg::Twist message;  
 
-    rclcpp::WallRate loop_rate(500ms);
+    rclcpp::WallRate loop_rate(10ms);
+    int i = 0, n = 10;
+    while(rclcpp::ok() && i < n){
+    	i++;
+    	message.linear.x = 1.0;
+    }
     
+    message.linear.x = 0.0;
     while (rclcpp::ok()) { 
     	message.linear.x = 1.0;
     	message.angular.z = 1.0;
