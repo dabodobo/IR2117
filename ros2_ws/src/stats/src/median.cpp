f8153e494cb435699a15c8f18fcab63fc76b7a90
davidroman <david.roman.grijalbo@gmail.com>
2025-02-06 11:53:39 +0100

    primer intento de median + cmakelist actualizado

--- ros2_ws/src/stats/src/median.cpp
+++ ros2_ws/src/stats/src/median.cpp
@@ -2,22 +2,28 @@
 #include "std_msgs/msg/float32.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include <vector>
+#include <algorithm>
 
 
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32>> publisher;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32>> publisher; //declaro el subscriptor para el callback
 std::vector<float> numeros;
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){ 
-	
-    float num = msg->data;
-    
+
+    std::vector<float> ordenados;
+    float median;
     std_msgs::msg::Float32 out_msg;
     
-    out_msg.data = mean;
+    float num = msg->data;
+    numeros.push_back(num);
+    
+    ordenados = std::sort(numeros.begin(), numeros.end());
+    median = ordenados[ordenados.size()/2];
+    
+    out_msg.data = median;
     publisher->publish(out_msg);
 }
 
 int main(int argc, char * argv[]){
-    count = 0;
 	
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("median"); //creo el nodo media
