135c2f87fa91a690ad8a30b1b30cb007ca94650f
davidroman <david.roman.grijalbo@gmail.com>
2025-02-06 11:47:08 +0100

    Median creado

--- ros2_ws/src/stats/src/median.cpp
+++ ros2_ws/src/stats/src/median.cpp
@@ -1,17 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
 #include "std_msgs/msg/int32.hpp"
-#include <iostream>
+#include <vector>
 
-float sum;
-int count;
+
 std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32>> publisher;
-
+std::vector<float> numeros;
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){ 
-	count++;
-    sum += msg->data;
+	
+    float num = msg->data;
+    
     std_msgs::msg::Float32 out_msg;
-    float mean = sum / count;
+    
     out_msg.data = mean;
     publisher->publish(out_msg);
 }
@@ -20,11 +20,11 @@ int main(int argc, char * argv[]){
     count = 0;
 	
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("mean"); //creo el nodo media
+    auto node = rclcpp::Node::make_shared("median"); //creo el nodo media
     auto subscription = 
         node->create_subscription<std_msgs::msg::Int32>(
             "number", 10, topic_callback); //me suscribo a topic
-    publisher = node->create_publisher<std_msgs::msg::Float32>("t_mean", 10); //publico la media
+    publisher = node->create_publisher<std_msgs::msg::Float32>("topic_median", 10); //publico la media
 
     rclcpp::spin(node); 
     rclcpp::shutdown();
