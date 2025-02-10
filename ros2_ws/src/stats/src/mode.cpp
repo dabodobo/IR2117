0aa749de2d7bf921a906086cddb9849fb871ce11
davidroman <david.roman.grijalbo@gmail.com>
2025-02-10 10:25:42 +0100

    cmake list updated

---
+++ ros2_ws/src/stats/src/mode.cpp
@@ -0,0 +1,45 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include <map>
+#include <algorithm>
+
+
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32>> publisher; //declaro el subscriptor para el callback
+std::map<float,int> counter;
+
+void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){ 
+
+    
+    float num = msg->data;
+    counter[num]+=1;
+    int mayor = 0;
+    float valor_mayor;
+    for(int i : counter ){
+    	if(counter[i] > mayor){
+    		mayor = counter[i];
+    		valor_mayor = i;
+    	}
+    
+    }
+    
+    std_msgs::msg::Float32 out_msg;
+    out_msg.data = valor_mayor;
+    publisher->publish(out_msg);
+}
+
+int main(int argc, char * argv[]){
+	
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("median"); //creo el nodo media
+    auto subscription = 
+        node->create_subscription<std_msgs::msg::Int32>(
+            "number", 10, topic_callback); //me suscribo a topic
+    publisher = node->create_publisher<std_msgs::msg::Float32>("topic_mode", 10); //publico la media
+
+    rclcpp::spin(node); 
+    rclcpp::shutdown();
+    return 0;
+}
+
+
