27f63e6313545f3f6417a9820131a456e5c8ac76
davidroman <david.roman.grijalbo@gmail.com>
2025-02-11 11:36:01 +0100

    Empiezo con el square

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -8,25 +8,37 @@ int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv); 
     auto node = rclcpp::Node::make_shared("publisher"); 
     auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10); 
-    geometry_msgs::msg::Twist message;  
+    
+    geometry_msgs::msg::Twist message;  //creo el mensaje cmd_vel
 
     rclcpp::WallRate loop_rate(10ms);
-    int i = 0, n = 10;
-    while(rclcpp::ok() && i < n){
-    	i++;
-    	message.linear.x = 1.0;
-    }
+    
+    int i = 0, n = 1000 // como voy a 0.1 m/s y hago un looprate de 10ms entonces para hacer un metro a una velocidad de 1 metro por segundo
+    // necesito hacer 1000 iteraciones
     
     message.linear.x = 0.0;
-    while (rclcpp::ok()) { 
-    	message.linear.x = 1.0;
-    	message.angular.z = 1.0;
+    for(int j = 0; j < 4; j++){
+    while (rclcpp::ok() && i < n) { 
+    	i++;
+    	message.linear.x = 10.0;
         publisher->publish(message);
         rclcpp::spin_some(node); 
         loop_rate.sleep(); 
     }
-
+     message.linear.x = 0.0; // lo paro antes de girar
+    publisher->publish(message);
+    std::spin(node);
+   
+    int i = 0, n = 100 
+    
+    while(rclcpp::ok() && i < n;)
+    	i++;
+	message.angular.z = j % 2 == 0? -1.0; 1.0;
+	publisher->publish(message);
+	rclcpp::spin_some(node); 
+        loop_rate.sleep();
     rclcpp::shutdown(); 
+    }
     return 0;
 }
 
