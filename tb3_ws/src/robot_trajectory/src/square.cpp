8ddbee50292e055c01320797859632245075bce7
davidroman <david.roman.grijalbo@gmail.com>
2025-02-11 12:05:45 +0100

    hace el cuadrado

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -13,32 +13,41 @@ int main(int argc, char *argv[]) {
 
     rclcpp::WallRate loop_rate(10ms);
     
-    int i = 0, n = 1000 // como voy a 0.1 m/s y hago un looprate de 10ms entonces para hacer un metro a una velocidad de 1 metro por segundo
-    // necesito hacer 1000 iteraciones
+ 
     
     message.linear.x = 0.0;
+    
     for(int j = 0; j < 4; j++){
+    
+    	int i = 0, n = 1000 ;
+    	
     while (rclcpp::ok() && i < n) { 
     	i++;
-    	message.linear.x = 10.0;
+    	message.linear.x = 0.1;
         publisher->publish(message);
         rclcpp::spin_some(node); 
         loop_rate.sleep(); 
     }
      message.linear.x = 0.0; // lo paro antes de girar
     publisher->publish(message);
-    std::spin(node);
+    rclcpp::spin_some(node);
    
-    int i = 0, n = 100 
+    i = 0; n = 1000; 
     
-    while(rclcpp::ok() && i < n;)
+    while(rclcpp::ok() && i < n){
     	i++;
-	message.angular.z = j % 2 == 0? -1.0; 1.0;
+	message.angular.z = 0.16;
 	publisher->publish(message);
 	rclcpp::spin_some(node); 
         loop_rate.sleep();
-    rclcpp::shutdown(); 
+        }
+    message.angular.z = 0.0;
+    publisher->publish(message);
+    rclcpp::spin_some(node); 
+    loop_rate.sleep();
+    
     }
+    rclcpp::shutdown(); 
     return 0;
 }
 
