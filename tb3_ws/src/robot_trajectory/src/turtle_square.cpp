956f6173eebb39bd5022ccfdc200c23bf1430654
davidroman <david.roman.grijalbo@gmail.com>
2025-02-12 13:21:04 +0100

    He creado un programa en especifico para mover la tortuga y no hacer el remap. Ahora me va bien el cuadrado

--- tb3_ws/src/robot_trajectory/src/turtle_square.cpp
+++ tb3_ws/src/robot_trajectory/src/turtle_square.cpp
@@ -6,8 +6,8 @@ using namespace std::chrono_literals;
 
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv); 
-    auto node = rclcpp::Node::make_shared("publisher"); 
-    auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10); 
+    auto node = rclcpp::Node::make_shared("turtle_square"); 
+    auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10); 
     
     geometry_msgs::msg::Twist message;  //creo el mensaje cmd_vel
 
@@ -19,11 +19,11 @@ int main(int argc, char *argv[]) {
     
     for(int j = 0; j < 4; j++){
     
-    	int i = 0, n = 1000;	
+    	int i = 0, n = 200;	
     	
     while (rclcpp::ok() && i < n) { 
     	i++;
-    	message.linear.x = 0.1;
+    	message.linear.x = 2.0;
         publisher->publish(message);
         rclcpp::spin_some(node); 
         loop_rate.sleep(); 
@@ -32,11 +32,11 @@ int main(int argc, char *argv[]) {
     publisher->publish(message);
     rclcpp::spin_some(node);
    
-    i = 0; n = 1000; 
+    i = 0; n = 100; 
     
     while(rclcpp::ok() && i < n){
     	i++;
-	message.angular.z = 0.16;
+	message.angular.z = 1.57; //1 rad/seg
 	publisher->publish(message);
 	rclcpp::spin_some(node); 
         loop_rate.sleep();
