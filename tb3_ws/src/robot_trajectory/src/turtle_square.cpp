70df9b0bcc416e75d12e5b15946c65d1e56d6b29
davidroman <david.roman.grijalbo@gmail.com>
2025-02-18 11:37:30 +0100

    los parametros actualizados, practica finalizada.

--- tb3_ws/src/robot_trajectory/src/turtle_square.cpp
+++ tb3_ws/src/robot_trajectory/src/turtle_square.cpp
@@ -14,12 +14,20 @@ int main(int argc, char *argv[]) {
 
     rclcpp::WallRate loop_rate(10ms);
     
+     message.linear.x = 0.0;
  
-    double linear_speed = 0.1;
-    double angular_speed = 0.1;
-    int square_length = 4;
+   // estos son base, si o se declara
+    node->declare_parameter("linear_speed",0.1);
+    node->declare_parameter("angular_speed",0.1);
+    node->declare_parameter("square_length",4.0);
     
-    message.linear.x = 0.0;
+    // si se los pasamos
+    
+    double linear_speed = node -> get_parameter("linear_speed").get_parameter_value().get<double>();
+    double angular_speed = node -> get_parameter("angular_speed").get_parameter_value().get<double>();
+    double square_length = node -> get_parameter("square_length").get_parameter_value().get<double>();
+   
+    
     for(int j = 0; j < 4; j++){
     
     int i = 0, n = square_length / (0.01 * linear_speed);	
