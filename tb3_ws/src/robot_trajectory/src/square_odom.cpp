89c739f118e5881e17422e9ddb993fb8ae0e3f85
davidroman <david.roman.grijalbo@gmail.com>
2025-02-20 13:06:57 +0100

    display de grados

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -6,6 +6,12 @@
 #include "map"
 
 
+float quat(float x, float y, float z, float w){
+	float siny_cosp = 2 *(w*z + x*y);
+	float cosy_cosp = 1 - 2*(y*y +z*z);
+	double yaw = std::atan2(siny_cosp, cosy_cosp); 
+	return yaw;
+}
 using namespace std::chrono_literals;
 //std::shared_ptr<rclcpp::Publisher<std_msgs::msg::String>> publisher
 std::map<std::string,float> position;
@@ -13,14 +19,24 @@ bool first_read = true;
 
 void topic_callback(const nav_msgs::msg::Odometry msg){
 	/*if (fisrt_read){
-	    initial_position= {"x" : nav_msgs::msg::Odometry.pose.pose.position.x, "y" : nav_msgs::msg::Odometry.pose.pose.position.y }
+	    position["x"] = msg.pose.pose.position.x;
+	    position["y"] = msg.pose.pose.position.y;
 	}
 	*/
+	
 	position["x"] = msg.pose.pose.position.x;
 	position["y"] = msg.pose.pose.position.y;
+	
+	float angle_x = msg.pose.pose.orientation.x;
+	float angle_y = msg.pose.pose.orientation.y;
+	float angle_z = msg.pose.pose.orientation.z;
+	float angle_w = msg.pose.pose.orientation.w;
+	
+	position["angle"] = quat(angle_x,angle_y,angle_z,angle_w);
 	 
 	std::cout << "Position x: " << position["x"] << std:: endl;
 	std::cout << "Position y: " << position["y"] << std:: endl;
+	std::cout << "Angle : " << position["angle"] << std::endl;
 	std::cout << "- - - - - - - - - - - " << std::endl << std::endl;
 	
 }
