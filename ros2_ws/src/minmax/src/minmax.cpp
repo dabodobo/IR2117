970f97cd36015117e2e27f53165309f888c7c7a8
dabodobo <david.roman.grijalbo@gmail.com>
2025-02-24 10:08:45 +0100

    Solucion con MultiArray

--- ros2_ws/src/minmax/src/minmax.cpp
+++ ros2_ws/src/minmax/src/minmax.cpp
@@ -1,40 +1,51 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
 #include <iostream>
+#include <vector>
 
 
+
+int max;
+int min;
 int value;
+bool first_read = true;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32MultiArray>> publisher;
 
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>> publisher;
-
-void min_callback(const std_msgs::msg::Int32::SharedPtr msg){
- 
-    value = msg->data;    
-    std_msgs::msg::Int32 out_msg;
-    out_msg.data = value;
+void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){
+    std::vector<int> minmax;
+    value = msg->data;
+    
+     if(first_read){
+     	min = value;
+    	max = value;
+    	first_read = false;
+    }
+    if(value > max){
+    	max = value;
+    }
+    if(value < min){
+    	min = value;
+    }
+    
+    minmax.push_back(min);
+    minmax.push_back(max);
+    
+    std_msgs::msg::Int32MultiArray out_msg;
+    out_msg.data = minmax;
     publisher->publish(out_msg);
 }
 
-void max_callback(const std_msgs::msg::Int32::SharedPtr msg){
- 
-    value = msg->data;    
-    std_msgs::msg::Int32 out_msg;
-    out_msg.data = value;
-    publisher->publish(out_msg);
-}
+
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("minmax"); //creo el nodo minmax
     auto subscription_min = 
         node->create_subscription<std_msgs::msg::Int32>(
-            "min", 10, min_callback); //me suscribo a topic min
+            "number", 10, topic_callback); //me suscribo a topic min
     
-     auto subscription_max = 
-        node->create_subscription<std_msgs::msg::Int32>(
-            "max", 10, max_callback); //me suscribo a topic min
-    
-    publisher = node->create_publisher<std_msgs::msg::Int32>("minmax", 10); 
+    publisher = node->create_publisher<std_msgs::msg::Int32MultiArray>("minmax", 10); 
 
     rclcpp::spin(node); 
     rclcpp::shutdown();
