c67ef2eab17f529a05c5138a73f72f0b817a5980
dabodobo <david.roman.grijalbo@gmail.com>
2025-02-24 09:06:48 +0100

    Read an input message from the input topic,store data in a variable, and display it

--- ros2_ws/src/minmax/src/minmax.cpp
+++ ros2_ws/src/minmax/src/minmax.cpp
@@ -1,27 +1,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
-
+#include <iostream>
 
 int min;
+int max;
+int value;
+bool first_read = true;
 std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>> publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){ 
 	
-    sum += msg->data;
-    std_msgs::msg::Int32 out_msg;
-    float mean = sum / count;
-    out_msg.data = mean;
-    publisher->publish(out_msg);
+    value = msg->data;
+    std::cout << value;
+    //std_msgs::msg::Int32 out_msg;
+
+    //out_msg.data = value
+    //publisher->publish(out_msg);
 }
 
 int main(int argc, char * argv[]){
-    count = 0;
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("min"); //creo el nodo min
     auto subscription = 
         node->create_subscription<std_msgs::msg::Int32>(
             "number", 10, topic_callback); //me suscribo a topic
-    publisher = node->create_publisher<std_msgs::msg::Int32>("t_mean", 10); //publico la media
+    publisher = node->create_publisher<std_msgs::msg::Int32>("x", 10); //publico la media
 
     rclcpp::spin(node); 
     rclcpp::shutdown();
