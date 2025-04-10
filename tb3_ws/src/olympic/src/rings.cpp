a763b081c1e4562317598707bdd1a299cdfdd2dc
davidroman <david.roman.grijalbo@gmail.com>
2025-04-10 13:56:04 +0200

    Ring ya dibuja un circulo

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -3,23 +3,26 @@
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-using namespace std::chrono_literals;
+
 
 int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
- auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("turtle/cmd_vel", 10);
- geometry_msgs::msg::Twist message;
- auto publish_count = 0;
- rclcpp::WallRate loop_rate(500ms);
-
+ 
+ auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+ 
+ geometry_msgs::msg::Twist vel;
+ 
  while (rclcpp::ok()) {
-   message.linear.x = 1.0;
-   publisher->publish(message);
+   vel.linear.x = 1.0;
+   vel.angular.z = 1.0;
+   publisher->publish(vel);
    rclcpp::spin_some(node);
-   loop_rate.sleep();
+  }
+  
  rclcpp::shutdown();
  return 0;
+ 
 }
 
