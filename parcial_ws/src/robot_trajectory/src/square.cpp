f4fe7e8b03895e319aff8d2e5998821e4bc80fac
davidroman <david.roman.grijalbo@gmail.com>
2025-04-12 12:27:24 +0200

    Parcial: square finalizado

--- parcial_ws/src/robot_trajectory/src/square.cpp
+++ parcial_ws/src/robot_trajectory/src/square.cpp
@@ -1,27 +1,70 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include <chrono>
+#include <iostream>
 
 using namespace std::chrono_literals;
 int n = 0;
+
+void avanzar (geometry_msgs::msg::Twist& vel){
+  vel.linear.x  = 0.1;
+  vel.angular.z = 0.0;
+}
+void stop (geometry_msgs::msg::Twist& vel){
+  vel.linear.x  = 0.0;
+  vel.angular.z = 0.0;
+}
+void girar (geometry_msgs::msg::Twist& vel){
+  vel.linear.x  = 0.0;
+  vel.angular.z = 0.1;
+}
+
 int main(int argc, char* argv[]){
+  int n = 0;
   rclcpp::init(argc,argv);
   auto node = rclcpp::Node::make_shared("square");
   auto publisher = node -> create_publisher<geometry_msgs::msg::Twist>("/cmd_vel",10);
   geometry_msgs::msg::Twist vel;
   rclcpp::WallRate loop_rate(10ms);
   
-  while(rclcpp::ok() && n <= 1000){
-    vel.linear.x = 0.1;
+  for(int i = 0; i < 4; i++){
+  
+    while(rclcpp::ok() && n <= 1000){
+      avanzar(vel);
+      publisher -> publish(vel);
+      rclcpp::spin_some(node);
+      loop_rate.sleep();
+      n++;
+    
+    }
+      stop(vel);
+      publisher -> publish(vel);
+      rclcpp::spin_some(node);
+      n = 0;
+      
+    while(rclcpp::ok() && n <= 1570){
+      girar(vel);
+      publisher -> publish(vel);
+      rclcpp::spin_some(node);
+      loop_rate.sleep();
+      n++;
+    
+    }
+    
+      stop(vel);
+      publisher -> publish(vel);
+      rclcpp::spin_some(node);
+      n = 0;
+    
+  }
+  std::cout << "Acabado ... "<< std::endl;
+  for(int i = 0; i < 100; i++){
+    stop(vel);
     publisher -> publish(vel);
     rclcpp::spin_some(node);
     loop_rate.sleep();
-    n++;
-  
   }
-  vel.linear.x = 0.0;
-    publisher -> publish(vel);
-    rclcpp::spin_some(node);
+   std::cout << "Parado ... "<< std::endl;
   rclcpp::shutdown();
   return 0;
   
