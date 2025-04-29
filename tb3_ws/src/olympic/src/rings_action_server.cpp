a47ee33fd45343aa877bd60f49ef38c54aa90483
dabodobo <david.roman.grijalbo@gmail.com>
2025-04-29 12:53:48 +0200

    desde el action se imprime el radio

--- tb3_ws/src/olympic/src/rings_action_server.cpp
+++ tb3_ws/src/olympic/src/rings_action_server.cpp
@@ -46,42 +46,12 @@ void execute(const std::shared_ptr<GoalHandleRings> goal_handle){
   //auto& sequence = feedback->partial_sequence;
   
    // r = v / w
-  std::cout << goal_handle << std::endl;
+  
   
   auto result = std::make_shared<Rings::Result>();
+  std::cout << result << std::endl;
+ 
   
-  /*
-  for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
-  
-    // En caso de cancelación ...
-  
-    if ( goal_handle->is_canceling() ) {
-    
-      result->sequence = sequence;
-      goal_handle->canceled(result);
-      
-      RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Canceled");
-      
-      return;
-      
-    }
-    
-    //añadimos la suma de los 2 anteriores ...
-    sequence.push_back(sequence[i] + sequence[i - 1]);
-    
-    goal_handle->publish_feedback(feedback);
-    
-    RCLCPP_INFO(rclcpp::get_logger("server"), "Publish Feedback");
-    loop_rate.sleep();
-  }*/
-  
-  if (rclcpp::ok()) {
-  
-    //result->sequence = sequence;
-    //goal_handle->succeed(result);
-    RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Succeeded");
-    
-  }
 }
 
 
