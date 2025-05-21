cafdcafc19b4c9089628ee4594e3d675324132fc
dabodobo <david.roman.grijalbo@gmail.com>
2025-05-21 11:40:14 +0200

    launch file terminado

--- tb3_ws/src/intrusion/launch/monitor.launch.py
+++ tb3_ws/src/intrusion/launch/monitor.launch.py
@@ -8,31 +8,77 @@ def generate_launch_description():
 	Node(
 		package='obstacles',
 		executable='detector',
-		namespace='front',
+		namespace='north',
 		parameters=[
 			{"obs_angle_min": -0.3927},
 			{"obs_angle_max": 0.3927},
-			{"obs_threshold": 1.0}]
+			{"obs_threshold": 2.0}]
 	),
 	
 	Node(
 		package='obstacles',
 		executable='detector',
-		namespace='left',
+		namespace='west',
 		parameters=[
-			{"obs_angle_min": 0.3927},
-			{"obs_angle_max": 1.1781},
-			{"obs_threshold": 1.0}]
+			{"obs_angle_min": 1.3},
+			{"obs_angle_max": 1.8},
+			{"obs_threshold": 2.0}]
 	),
 	
 	Node(
 		package='obstacles',
 		executable='detector',
-		namespace='right',
+		namespace='east',
 		parameters=[
-			{"obs_angle_min": -1.1781},
-			{"obs_angle_max": -0.3927},
-			{"obs_threshold": 1.0}]
+			{"obs_angle_min": -1.8},
+			{"obs_angle_max": -1.3},
+			{"obs_threshold": 2.0}]
+	),
+	
+	Node(
+		package='obstacles',
+		executable='detector',
+		namespace='northwest',
+		parameters=[
+			{"obs_angle_min": 0.5},
+			{"obs_angle_max": 1.1},
+			{"obs_threshold": 2.0}]
+	)
+	Node(
+		package='obstacles',
+		executable='detector',
+		namespace='northeast',
+		parameters=[
+			{"obs_angle_min": -1.1},
+			{"obs_angle_max": -0.5},
+			{"obs_threshold": 2.0}]
+	),
+	Node(
+		package='obstacles',
+		executable='detector',
+		namespace='south',
+		parameters=[
+			{"obs_angle_min": 2.7},
+			{"obs_angle_max": -2.7},
+			{"obs_threshold": 2.0}]
+	),
+	Node(
+		package='obstacles',
+		executable='detector',
+		namespace='southwest',
+		parameters=[
+			{"obs_angle_min": 2.0},
+			{"obs_angle_max": 2.5},
+			{"obs_threshold": 2.0}]
+	),
+	Node(
+		package='obstacles',
+		executable='detector',
+		namespace='southeast',
+		parameters=[
+			{"obs_angle_min": -2.5},
+			{"obs_angle_max": -2.0},
+			{"obs_threshold": 2.0}]
 	)
 	
 
